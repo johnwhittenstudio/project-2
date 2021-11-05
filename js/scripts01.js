9c56cd2b90f871cf1f4e4597721456904435120c
John Whitten <johnwhitten.studio@gmail.com>
2021-11-05 15:51:52 -0700

    fix some js bugs.

--- js/scripts01.js
+++ js/scripts01.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   $("form#main").submit(function(event) {
+    const name = $("input#name").val();
     const number1 = parseInt($("select#animal").val());
+    const number2 = parseInt($("select#comfort").val());
+
 
   if (number1 === 1) {
     $("#python").toggle();
+    $(".name").text(name);
   } else if (number1 === 2) {
+    $(".name").text(name);
     $("#csharp").toggle();
   } else if (number1 === 3) {
+    $(".name").text(name);
     $("#rust").toggle()
   }
 
