33dd65e3407b991adf12ebb6fac252aadc156fda
John Whitten <johnwhitten.studio@gmail.com>
2021-11-04 18:41:18 -0700

    work to debug js issues.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
   $("form#main").submit(function(event) {
     const age = parseInt($("input#age").val());
-  });
+    console.log("age");
+
   $("form#main").submit(function(event) {  
     let animal = $("#animal").val();
     const number1 = parseInt($("select#animal").val());
-  });
+    console.log("animal");
+
   $("form#main").submit(function(event) {  
     let comfort = $("input:radio[name=comfort]:checked").val();
-    const number2 = parseInt($("select#comfort").val());
-  });
+    const number2 = parseInt($("input#comfort").val());
+    console.log("comfort");
+
     $("form#main").submit(function(event) {
     let color = $("#color").val();
     const number3 = parseInt($("input#color").val());
-  });
+    console.log("color");
 
-    const score = age + number1 + number2 + number3;
+
+    const score = age + number1 + number2;
     console.log(score);
 
-  });
+
+
+  if (score <= 19) {
+    score 
+  }
+
+  if (colorInput.includes("000000") === true)
+    score += 50;
+  else {
+    score += 10;
+
+
 
   if (score <= 79) {
     $("#python").show();
@@ -27,9 +42,7 @@ $(document).ready(function() {
     $("#csharp").show();
   } else if (value >= 160) {
     $("#rust").show();
-
-
     
     event.preventDefault();
   };
-;
+});
